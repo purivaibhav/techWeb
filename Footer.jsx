cc3c62a19b43243bb71b1480931b7c168e486184
vaibhavpuri369 <123762421+vaibhavpuri369@users.noreply.github.com>
2023-01-28 20:35:52 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@all right reserved</p>
+
+        </div>
+
+        <div>
+            <h5>Follow us</h5>
+            <div>
+                <a href="www.linkedin.com/in/vaibhav-puri-2296a5225/" target={"blank"}>Linkedin</a>
+                <a href="https://www.instagram.com/vaibhavpuri921/" target={"blank"}>Instagram</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
